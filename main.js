d3d8bae58079bfbff66fdc7af95f672f9e1a9960
Maggie McCausland <maggiemccausland@hotmail.com>
2021-09-20 16:46:40 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+document.getElementById("").addEventListener("click", displayDate);
+
+var toDos = {
+    name: '',
+    status: '',
+};
+
+var arr = [{
+    name: "Wake up",
+    status: "true",
+},
+{
+    name: "Make Bed",
+    status: "false",
+}
+];
+
+
+
+let btn = document.createElement("button");
+button.innerHTML = "Add";
+document.body.appendChild(btn);
