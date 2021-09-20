68dc4be0e8b376ce176d69d2ac5b5e8e4dcd93d6
Erin Hancock <erinhancock794@gmail.com>
2021-09-20 17:51:17 -0600

    commenting code

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 
 // document.getElementbyId("returnbtn").addEventListener("click", displayDate);
 
-var toDos = {
-    // name: '',
-    // status: '',
-};
+// var toDos = {
+//     // name: '',
+//     // status: '',
+// };
 
-var arr = [
-    {
-    name: "Wake up",
-    complete: true,
-},
-{
-    name: "Make Bed",
-    complete: false,
-}
-];
+// var arr = [
+//     {
+//     name: "Wake up",
+//     complete: true,
+// },
+// {
+//     name: "Make Bed",
+//     complete: false,
+// }
+// ];
 
-
-//test
 let taskInput = document.getElementById('new-task-input');
 
 let addBtn = document.getElementById('returnbtn');
@@ -48,17 +46,12 @@ let addTask = () => {
 
     let listItem=createNewTask(taskInput.value);
 
-	//Append listItem to incompleteTaskHolder
-	// incompleteTaskHolder.appendChild(listItem);
-	// bindTaskEvents(listItem, taskCompleted);
     incompleteUl.appendChild(listItem)
 
-	// taskInput.value="";
     console.log('taskInput--->', taskInput.value)
     return listItem;
 }
 
-// addButton.onclick=addTask;
 addBtn.addEventListener("click",addTask);
 /* let btn = document.createElement("button");
 button.innerHTML = "Add";
