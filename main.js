9c7887ee01292029c1eb1ed5549e273edaa11e15
Maggie McCausland <maggiemccausland@hotmail.com>
2021-09-20 16:53:42 -0600

    add event listener

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 
-document.getElementById("").addEventListener("click", displayDate);
+document.getElementbyId().addEventListener("click", displayDate);
 
 var toDos = {
     name: '',
     status: '',
 };
 
-var arr = [{
+var arr = [
+    {
+    id: 1,
     name: "Wake up",
     status: "complete",
 },
 {
+    id: 2,
     name: "Make Bed",
     status: "incomplete",
 }
