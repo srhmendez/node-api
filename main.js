df468292bc68db94528a3c63e8ae3298984f4469
Sarah Mendez <srhmendez@gmail.com>
2021-09-22 20:23:25 -0600

    fix incomplete / complete task lists

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let todoArray = [
     id: 1,
     name: "go to work",
     status: '',
-    complete: true
+    complete: false
   },
   {
     id: 2,
@@ -20,30 +20,54 @@ let todoArray = [
   },
 ];
 
-//adds array items on page load
+//adds array items from array above on page load to the addTask
 window.addEventListener("load", () => {
    
   todoArray.map((task) => {
-    addTask(task);
+    formatExistingJSON(task);
   });
 
 });
 
+
 //adds a new task object to the array
-function addTask(task) {
+function formatExistingJSON(task) {
+
   console.log('task--->', task);
   const todo = {
-    id: task.id ? task.id : todoArray.length,
+    id: task.id,
+    name: task.name,
+    status: '',
     complete: false,
-    name: task.name || task,
+    
   };
-  console.log('todo----->', todo);
-  //pushes existing to dos to Array with checked values
-  todoArray.push(todo);
 
   //creating a new task to put into the HTML DOM
   createNewTask(todo);
 }
+
+
+//Formats newly input ToDo
+const formatNewJSON = (inputString) =>{ 
+    console.log('task--->', inputString);
+
+
+    const todo = {
+    id: (todoArray.length + 1),
+    name: inputString,
+    status: '',
+    complete: false,
+  
+    };
+
+    //pushes existing to dos to Array with checked values
+    todoArray.push(todo);
+
+    //creating a new task to put into the HTML DOM
+    createNewTask(todo);
+}
+
+
 
 //turns input into text to be used to create a todo task
 function submitInput(event) { 
@@ -51,7 +75,7 @@ function submitInput(event) {
   const input = document.querySelector(".todo-list-input");
   const text = input.value.trim();
   if (text !== "") {
-    addTask(text);
+    formatNewJSON(text);
     input.value = "";
     input.focus();
   } else if (text == false) {
@@ -77,17 +101,26 @@ document
 
 //event listener for remove-items
 
-function removeItem() {
-  event.currentTarget.parentElement.remove();
+function removeTodo(key) {
+  event.currentTarget.parentElement.parentElement.parentElement.remove();
+  
 }
 
 //function to toggle between complete and incomplete tasks
 
-function toggleDone(key) {
+function toggleComplete(key) {
   const index = todoArray.findIndex((item) => item.id === Number(key));
-  todoArray[index].checked = !todoArray[index].checked;
+  if (todoArray[index].complete == false) {
+    todoArray[index].complete = true;
+  } else if (todoArray[index].complete == true) {
+    todoArray[index].complete = false;
+    createNewTask(key)
+    removeTodo(key)
+  }
 
     //create a render lists function that searched for the to do item that contains the key referenced above.
+    console.log('index-->', index)
+    findRemovedTask(key)
 
 }
 
@@ -95,15 +128,15 @@ function toggleDone(key) {
 //Creating Task to add to the HTML DOM
 function createNewTask(todo) {
   const incompleteList = document.querySelector("#incomplete-ul");
-  const completeList = document.querySelector("#complete-ul");
+  let completeList = document.querySelector("#complete-ul");
   const item = document.querySelector(`[data-key='${todo.id}']`);
-  console.log('item--', item)
+  
   let listItem = document.createElement("li");
   listItem.setAttribute("data-key", todo.id);
   listItem.innerHTML = `
   <div class="form-check">
   <label class="form-check-label">
-  <input id="${todo.id}" onClick="toggleDone(${todo.id})" class="js-tick checkbox" type="checkbox"/>
+  <input id="${todo.id}" onClick="toggleComplete(${todo.id})" class="js-tick checkbox" type="checkbox"/>
   ${todo.name}
   <p class="input-helper" id="incomplete-list">
   </p>
@@ -113,8 +146,14 @@ function createNewTask(todo) {
   </i>
 `;
 
-//adds newly created task to incomplete ToDo List Card
-incompleteList.append(listItem);
+// This checks if the newly created To Do is complete or not, if complete, it will append the item to the complete list and it will be toggled green. If it is false, and is not complete, it will add the item to the incomplete to do list.
+if (todo.complete == true) {
+    completeList.append(item);
+    console.log('todo array after item is checked off -->', todoArray)
+}else if (todo.complete == false){
+    //adds newly created task to incomplete ToDo List Card
+    incompleteList.append(listItem);
+}
 }
 
 
@@ -123,6 +162,16 @@ let promptError = (inputToDoString) => {
   if (inputToDoString === "") {
     window.alert("A To-Do item cannot be blank. Please try again.");
   }
+};
+
+const findRemovedTask = (key) =>{
+
+    todoArray.forEach(element => {
+        if (element.id == key) {
+            console.log(element)
+            createNewTask(element)
+        } 
+    })
 };
 
 
